69c21e3725b2a7d4c8cbab198edf04109257ddf1
francinedias <116805792+francinedias@users.noreply.github.com>
2024-03-21 08:28:25 -0300

    ok

---
+++ main.js
@@ -0,0 +1,15 @@
+const botoes = document.querySelectorAll (".botao");
+const textos = document.querySelectorAll(".aba-conteudo")
+
+for(let i=0; i <botoes.length;j++){
+    botoes[i].onclick = function (){
+        for(let j=0;j<botoes.length;j++){ 
+            botoes[j].classList.removem("ativo");
+            textos[j.classlist.remove("ativo")]
+
+}
+botoes[i].classList.add("ativo");
+botoes[i].classList.add("ativo");
+    }
+}
+contadores[0].textcontext = "contagem regressiva"
